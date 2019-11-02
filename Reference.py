e83b669ee75a8a7ac266bbe2a32b5f881c6c0d46
HongwuZ <957130821@qq.com>
2019-11-02 10:58:58 +0800

    1. 非线性可分数据集可视化代码来源 https://www.kaggle.com/mgabrielkerr/visualizing-knn-svm-and-xgboost-on-iris-dataset 2. 线性可分数据集可视化未完成

--- Reference.py
+++ Reference.py
@@ -1,103 +1,22 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-fig,ax=plt.subplots(figsize=(6,6))
-import numpy as np
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
 
-data=pd.read_csv(r"...\data1.csv",header=None)
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
 
-X=data.iloc[:,[0,1]]  #提取特征
-y=data[2]   #提取目標
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
 
-###把數據歸一化###
-mean=X.mean(axis=0)
-sigma=X.std(axis=0)
-X=(X-mean)/sigma
-
-###提取不同類別的數據，用於畫圖###
-x_positive=X[y==1]
-x_negative=X[y==-1]
-
-ax.scatter(x_positive[0],x_positive[1],marker="o",label="y=+1")
-ax.scatter(x_negative[0],x_negative[1],marker="x",label="y=-1")
-ax.legend()
-ax.set_xlabel("x1")
-ax.set_ylabel("x2")
-ax.set_title("Standardized Data")
-ax.set_xlim(-2,2.6)
-ax.set_ylim(-2,2.6)
-
-X[2]=np.ones((X.shape[0],1))   #給特征增加一列常數項
-X=X.values    #把特征轉換成ndarray格式
-
-###初始化w###
-w=X[0].copy()  #選取原點到第一個點的向量作為w的初始值
-w[2]=0  #增加一項---閾值，閾值初始化為0
-w=w.reshape(3,1)
-
-y=y.values.reshape(100,1)  #把目標轉換成ndarray格式，形狀和預測目標相同
-    
-def compare(X,w,y):
-    ###用於比較預測目標y_pred和實際目標y是否相符，返回分類錯誤的地方loc_wrong###
-    ###輸入特征，權重，目標###
-    scores=np.dot(X,w)  #把特征和權重點乘，得到此參數下預測出的目標分數
-    
-    y_pred=np.ones((scores.shape[0],1))  #設置預測目標，初始化值全為1，形狀和目標分數相同
-    
-    loc_negative=np.where(scores<0)[0]  #標記分數為負數的地方
-    y_pred[loc_negative]=-1  #使標記為負數的地方預測目標變為-1
-    
-    loc_wrong=np.where(y_pred!=y)[0]  #標記分類錯誤的地方
-    
-    return loc_wrong
-
-def update(X,w,y):
-    ###用於更新權重w，返回更新後的權重w###
-    ###輸入特征，權重，目標###
-    w=w+y[compare(X,w,y)][0]*X[compare(X,w,y),:][0].reshape(3,1)
-    return w
-
-def perceptron(X,w,y):
-    ###感知機算法，顯示最終的權重和分類直線，並畫出分類直線###
-    ###輸入特征，初始權重，目標###
-    while len(compare(X,w,y))>0:
-        print("錯誤分類點有{}個。".format(len(compare(X,w,y))))
-        w=update(X,w,y)
-
-    print("參數w:{}".format(w))
-    print("分類直線:{}x1+{}x2+{}=0".format(w[0][0],w[1][0],w[2][0]))
-    line_x=np.linspace(-3,3,10)
-    line_y=(-w[2]-w[0]*line_x)/w[1]
-    ax.plot(line_x,line_y)
-
-plt.show()
-
-
-
-
-def perceptron_pocket(X,w,y):
-    ###感知機口袋算法，顯示n次叠代後最好的權重和分類直線，並畫出分類直線###
-    ###輸入特征，初始權重，目標###
-    best_len=len(compare(X,w,y))  #初始化最少的分類錯誤點個數
-    best_w=w  #初始化口袋裏最好的參數w
-    for i in range(100):
-        print("錯誤分類點有{}個。".format(len(compare(X,w,y))))
-        w=update(X,w,y)
-        #如果當前參數下分類錯誤點個數小於最少的分類錯誤點個數，那麽更新最少的分類錯誤點個數和口袋裏最好的參數w
-        if len(compare(X,w,y))<best_len:
-            best_len=len(compare(X,w,y))
-            best_w=w
-
-    print("參數best_w:{}".format(best_w))
-    print("分類直線:{}x1+{}x2+{}=0".format(best_w[0][0],best_w[1][0],best_w[2][0]))
-    print("最少分類錯誤點的個數:{}個".format(best_len))
-    line_x=np.linspace(-3,3,10)
-    line_y=(-best_w[2]-best_w[0]*line_x)/best_w[1]
-    ax.plot(line_x,line_y)
-
-def update(X,w,y):
-    ###用於更新權重w，返回更新後的權重w###
-    ###輸入特征，權重，目標###
-    num=len(compare(X,w,y)) #分類錯誤點的個數
-    w=w+y[compare(X,w,y)][np.random.choice(num)]*X[compare(X,w,y),:][np.random.choice(num)].reshape(3,1)
-    return w
-
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
--- SVM.py
+++ SVM.py
@@ -1,24 +1,60 @@
 import pandas as pd
-import numpy as py
+import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split, LeaveOneOut, KFold, RepeatedKFold
 
 DEFINE_NON_LINEARABLE = 1
-# 0: 按比例划分 1：留一法 2：P次K折
-DEFINE_DATA_SPLIT = 0
+DEFINE_VISUALIZE = 1  # 是否开启可视化。如果开启可视化，在Irish数据集中，只能选择两种属性，如此方能画在二维平面上。
+DEFINE_DATA_SPLIT = 0  # 0: 按比例划分 1：留一法 2：P次K折
+
+
+def svm_show(X, y, classifier, resolution=0.02):
+    '''
+    :param X: 训练数据集
+    :param y: 真实数据标签
+    :param classifier: 训练完毕的SVM
+    :param resolution: 图中的最小间距
+    :return: void
+    '''
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contour(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl)
+
+    plt.show()
+
 
 if __name__ == '__main__':
+    # region 线性和非线性数据的读取
     if DEFINE_NON_LINEARABLE == 0:
         data = pd.read_csv('bill_authentication.csv')
     else:
         colnames = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
         data = pd.read_csv('iris.data', names=colnames)
 
-    X = data.drop('Class', axis=1)
-    y = data['Class']
+        iris = datasets.load_iris()
 
+    X = data.drop('Class', axis=1) if DEFINE_VISUALIZE == 0 else iris.data[:, [2, 3]]
+    y = data['Class'] if DEFINE_VISUALIZE == 0 else iris.target
+    # endregion
+
+    # region 数据划分
     if DEFINE_DATA_SPLIT == 0:
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     elif DEFINE_DATA_SPLIT == 1:
@@ -30,16 +66,27 @@ if __name__ == '__main__':
         # kf = RepeatedKFold(n_splits=3, n_repeats=3, random_state=0)
         train, test = kf.split(X)
         X_train, X_test, y_train, y_test = X[train], X[test], y[train], y[test]
+    # endregion
 
+    # region 归一化
+    sc = StandardScaler()
+    sc.fit(X_train)
+    X_train, X_test = sc.transform(X_train), sc.transform(X_test)
+    # endregion
+
+    # region 核函数选择
     if DEFINE_NON_LINEARABLE == 0:
         svc_classifier = SVC(kernel='linear')
     else:
         svc_classifier = SVC(C=1.0, kernel='poly', degree=8)
         # svc_classifier = SVC(C=1.0, kernel='rbf')
         # svc_classifier = SVC(C=1.0, kernel='sigmoid')
+    # endregion
 
     svc_classifier.fit(X_train, y_train)
-
+    # HACK: 是否根据可视化，把以下代码封装进svm_show函数中
     y_pred = svc_classifier.predict(X_test)
-
     print(classification_report(y_test, y_pred))
+
+    if DEFINE_VISUALIZE:
+        svm_show(X_test, y_test, svc_classifier)
